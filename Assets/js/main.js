2dd4cd6032cd24a19fc832fad2e07902527c0bac
CodeWithHelena <vivimarny@gmail.com>
2023-10-24 17:24:56 +0100

    project added

---
+++ Assets/js/main.js
@@ -0,0 +1,21 @@
+// const toggler = document.querySelector(".fa-bars");
+// const navList = document.querySelector(".nav-list");
+
+// if (toggler) {
+//   toggler.addEventListener("click", () => {
+//     navList.classList.toggle("open");
+//     console.log("clicked");
+//   });
+// } 
+// function toggler(){
+// 	const navbar= document.getElementById("nav-list");
+// 	navbar.classList.toggle("show");
+// 	console.log("clicked")
+// }
+function myfunction(){
+	// const message = document.getElementById("collection");
+	// message.textContent = "this is javaScript collection"
+
+			var navbar = document.getElementById('nav-list');
+			navbar.classList.toggle('show');
+}
